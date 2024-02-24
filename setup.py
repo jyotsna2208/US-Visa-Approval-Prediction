1371b46c1015a0536e5aac7572e36689cfbf8129
jyotsna2208 <jyotsna.seth@gmail.com>
2024-02-24 18:40:10 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Jyotsna",
+    author_email="jyotsna.seth@gmail.com",
+    packages=find_packages()
+)
