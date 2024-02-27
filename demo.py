358d9a8badace6f54171b183e2b3fa016bfc9414
jyotsna2208 <jyotsna.seth@gmail.com>
2024-02-27 21:35:25 +0530

    exception written

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("Starting")
+try:
+    a=1/"10"
+except Exception as e:
+    raise USvisaException(e,sys) from e
